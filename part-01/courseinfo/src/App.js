3cb3c211a6b868e0e9ec79e6fa6eb5021128dab8
elyas-kosari <elyas.kosari@gmail.com>
2021-10-23 19:40:54 -0400

    Add Part components

--- part-01/courseinfo/src/App.js
+++ part-01/courseinfo/src/App.js
@@ -12,7 +12,7 @@ const Part = (props) => {
   return (
     <div>
       <p>
-      {props.part} {props.exercises}
+        {props.part} {props.exercises}
       </p>
     </div>
   )
@@ -21,15 +21,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
